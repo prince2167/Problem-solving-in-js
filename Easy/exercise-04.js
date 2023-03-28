91c2195334d5930bbda2c543664ef54300b49915
Prince <singhprince8222692@gmail.com>
2023-03-28 12:19:30 +0530

    Exercise-04

---
+++ Easy/exercise-04.js
@@ -0,0 +1,11 @@
+// write a function that takes any numbers of arguments of type number and returns the sum of all arguments
+const sumOfArguments = (...args) => {
+  let sum = 0;
+  for (let i = 0; i < args.length; i++) {
+    sum += args[i];
+  }
+
+  return sum;
+};
+
+console.log(sumOfArguments(2, 3, 4, 5));
