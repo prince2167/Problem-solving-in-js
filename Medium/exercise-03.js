a19d0650927042908def3e134f4393bbabcf49ef
Prince <singhprince8222692@gmail.com>
2023-03-31 20:18:41 +0530

    Exercise-03

--- Medium/exercise-03.js
+++ Medium/exercise-03.js
@@ -1,14 +1,16 @@
 // create a function which takes a sentence as a parameter, and returns the longest word  in the sentence
 
-const longestWord = (str) => {
-  let word = str.split(" ");
-  let longestWord = [];
-  for (let i = 0; i < word.length; i++) {
-    if (longestWord.length[i] < word[i].length) {
-      longestWord.push(word[i]);
-    } else null;
+const longestWord = (string) => {
+  let str = string.split(" ");
+  let longest = 0;
+  let word = null;
+  for (let i = 0; i < str.length - 1; i++) {
+    if (longest < str[i].length) {
+      longest = str[i].length;
+      word = str[i];
+    }
   }
-  return longestWord;
+  return word;
 };
 
 console.log(longestWord("Hi this prince"));
