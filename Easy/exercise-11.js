c058bc0330bd098b27fc14183c96c542fa6dc479
Prince <singhprince8222692@gmail.com>
2023-04-01 22:13:02 +0530

    Exercise-11

---
+++ Easy/exercise-11.js
@@ -0,0 +1,14 @@
+// 3. Write an ES6 function to reverse a string in JavaScript without using in-built methods.
+
+ const reverseString = (str) => {
+   
+    let reverseString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+      reverseString += str[i];
+    }
+  
+    return reverseString;
+  };
+  
+  console.log(reverseString("Hello my name is prince")); 
+  
