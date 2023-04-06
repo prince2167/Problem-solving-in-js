32d655ede85da525cf203cb4274447009bb412a9
Prince <singhprince8222692@gmail.com>
2023-04-06 22:01:25 +0530

    Exercise-21

--- Easy/exercise-21.js
+++ Easy/exercise-21.js
@@ -1,7 +1,20 @@
 // Given an array of strings, use the reduce function to find the total length of all the strings combined.
-
 const strings = ["hello", "world", "how", "are", "you"];
 
 const totalLength = strings.reduce((acc, curr) => (acc += curr.length), 0);
 
 console.log(totalLength);
+// Given an array of objects representing students, use the reduce function to find the student name with the highest grade.
+
+const students = [
+  { name: "Alice", grade: 85 },
+  { name: "Bob", grade: 70 },
+  { name: "Charlie", grade: 90 },
+  { name: "Dave", grade: 80 },
+];
+
+const highestGrade = students.reduce((acc, curr) =>
+  acc.grade > curr.grade ? acc : curr
+);
+
+console.log(highestGrade);
